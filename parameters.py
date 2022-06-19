86f59ae477f632086f9984f2fbc39592be84a7b7
Julien-Sahli <julien.sahli@epfl.ch>
2022-06-19 23:01:15 +0200

    params update

--- parameters.py
+++ parameters.py
@@ -6,7 +6,7 @@ PARAMETERS
 INPUT_MIDI_DEVICE = 1
 OUTPUT_DEVICE = "pulse" # sd.default.device
 
-DEBUG = True
+DEBUG = False
 SAMPLING_FREQUENCY = 44100
 NYQUIST_FREQUENCY = SAMPLING_FREQUENCY/2
 SAMPLES_PER_FRAME = 512
@@ -16,10 +16,10 @@ GENERAL_VOLUME = 0.1
 # 0 = Equal Temperament
 # 1 = Pythagorean Tuning
 # 2 = Just Intonation
-TEMPERAMENT = 2
+TEMPERAMENT = 0
 
 REF_NOTE = 69 # midi code of A4
-REF_FREQUENCY = 880 # frequency of A4
+REF_FREQUENCY = 440 # frequency of A4
 
 """
 EQUAL TEMPERAMENT
